e8d34a0db9f0090027937edfd035f9e273add0c5
Filipe Campos <filipe_sobre1@hotmail.com>
2024-08-18 21:43:54 -0300

    Finalizado exercicio 53

---
+++ src/main/java/PacoteBase/Exercicio53.java
@@ -0,0 +1,19 @@
+package PacoteBase;
+
+import java.util.Scanner;
+
+public class Exercicio53 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("### Tabuada ###");
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        scanner.close();
+    }
+}
