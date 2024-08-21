c6b30736f81fefb512a438485072eab111edb9b3
beatrizx2 <beaazevedo14@gmail.com>
2024-08-21 14:40:37 -0300

    atualizacao questao 3

--- src/main/java/Exercicios1ao13BeatrizLista2/Q3.java
+++ src/main/java/Exercicios1ao13BeatrizLista2/Q3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Q3 {
 	public static void main(String[] args) {
 		   Scanner sc= new Scanner (System.in);
-		   System.out.println("Informe o número de mulheeres:");
+		   System.out.println("Informe o número de mulheres:");
 		   int mulheres= sc.nextInt();
 		   System.out.println("Informe o número de homens:");
 		   int homens= sc.nextInt();
